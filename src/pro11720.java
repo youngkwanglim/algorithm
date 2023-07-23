7295212764cd996f6a9122f49447a97e33728418
Youngkwang Lim <richard08@naver.com>
2023-07-23 23:18:25 +0900

    first commit

---
+++ src/pro11720.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class pro11720 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int count = sc.nextInt();
+        int a = sc.nextInt();
+        String k = Integer.toString(a);
+
+//        String k = sc.nextLine();
+        int sum = 0;
+
+        for (int i = 0; i < count; i++) {
+            int h = Integer.parseInt(String.valueOf(k.charAt(i)));
+//            int k = Integer.parseInt(b.substring(i));
+            sum += h;
+        }
+
+        System.out.println(sum);
+    }
+}
