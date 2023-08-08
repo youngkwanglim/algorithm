a8962faa2c5a00aaec70c27090ae673845aa674f
Youngkwang Lim <richard08@naver.com>
2023-08-08 23:24:25 +0900

    ReadyToCodingTest

---
+++ src/pro27866.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class pro27866 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+        int num = sc.nextInt();
+
+        System.out.println(a.charAt(num - 1));
+    }
+}
