18e3223a28fb826f211f664e9b7259f95f6a9a44
Youngkwang Lim <richard08@naver.com>
2023-08-14 23:43:46 +0900

    ReadyToCodingTest

--- src/pro1008.java
+++ src/pro1008.java
@@ -8,6 +8,7 @@ public class pro1008 {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
+
         System.out.println(a / b + a % b);
         System.out.println(a / b + a % b);
     }
