0e2678a6360b1d55b8af96afd179d8d8723caf37
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2023-09-21 14:30:18 +0900

    [Bronze I] Title: 평균, Time: 256 ms, Memory: 18920 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int count = sc.nextInt();
+        int[] major = new int[count];
+        for (int i = 0; i < count; i++) {
+            major[i] = sc.nextInt();
+        }
+
+        int max = 0;
+        for (int i = 0; i < count; i++) {
+            if (major[i] > max) {
+                max = major[i];
+            }
+        }
+
+        double[] major2 = new double[count];
+        double sum=0;
+        for (int i = 0; i < count; i++) {
+            major2[i] = ((double) major[i] / max) * 100;
+            sum += major2[i];
+        }
+
+        System.out.println(sum / count);
+    }
+}
