f674bb7a9b32ecce86d2a0f48efd9b505c986a82
Youngkwang Lim <richard08@naver.com>
2023-09-21 14:05:59 +0900

    ReadyToCodingTest

---
+++ src/clear/pro1008.java
@@ -0,0 +1,15 @@
+package clear;
+
+import java.util.Scanner;
+
+public class pro1008 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println((double) a / b);
+    }
+}
--- src/clear/pro10998.java
+++ src/clear/pro10998.java
@@ -1,13 +1,14 @@
+package clear;
+
 import java.util.Scanner;
 
-public class pro1008 {
-
+public class pro10998 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        System.out.println((double) a / b);
+        System.out.println(a * b);
     }
 }
