8b53a2437e7547ee5ae458be62179742057b5f68
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2023-11-09 18:17:11 +0900

    [level 1] Title: 내적, Time: 0.04 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -1,11 +1,9 @@
 class Solution {
     public int solution(int[] a, int[] b) {
-        int answer = 1234567890;
-        int sum=0;
+        int answer = 0;
         for(int i=0 ; i<a.length; i++){
-            sum += a[i] * b[i];
+            answer += a[i] * b[i];
         }
-        answer=sum;
         return answer;
     }
 }
