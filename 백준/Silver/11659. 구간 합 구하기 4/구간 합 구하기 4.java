5c23e01784da2375a8a0cf0cbe819f4d50a01064
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-05-23 10:59:21 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2532 ms, Memory: 258424 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -8,24 +8,23 @@ public class Main {
         int b = sc.nextInt();
 
         int[] list = new int[a];
-        int[] prefixSum = new int[a + 1]; // 누적 합을 저장할 배열. 배열의 크기를 a+1로 설정해서 인덱스 계산을 용이하게 함.
+        int[] sum = new int[a+1];
 
         for (int i = 0; i < a; i++) {
             list[i] = sc.nextInt();
-            if (i == 0) {
-                prefixSum[i + 1] = list[i]; // 첫 번째 원소의 누적 합은 자기 자신
-            } else {
-                prefixSum[i + 1] = prefixSum[i] + list[i]; // 현재 원소까지의 누적 합을 계산
+            if (i == 0){
+                sum[i + 1] = list[i];
+            }else {
+                sum[i + 1] = sum[i] +list[i];
             }
-        }
+         }
 
         for (int i = 0; i < b; i++) {
-            int c = sc.nextInt();
+            int c = sc.nextInt() - 1;
             int d = sc.nextInt();
-            // c부터 d까지의 구간 합을 계산하기 위해 prefixSum[d] - prefixSum[c-1]을 계산
-            System.out.println(prefixSum[d] - prefixSum[c - 1]);
-        }
 
-        sc.close();
+            int f = sum[d] - sum[c];
+            System.out.println(f);
+        }
     }
 }
