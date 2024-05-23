ce57db54791ced8e83e13337935376fee448ef08
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-05-23 19:22:51 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1600 ms, Memory: 59944 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,17 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
         int[] list = new int[a];
         int[] sum = new int[a+1];
 
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < a; i++) {
-            list[i] = sc.nextInt();
+            list[i] = Integer.parseInt(st.nextToken());
             if (i == 0){
                 sum[i + 1] = list[i];
             }else {
@@ -19,9 +24,11 @@ public class Main {
             }
          }
 
+        
         for (int i = 0; i < b; i++) {
-            int c = sc.nextInt() - 1;
-            int d = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int c = Integer.parseInt(st.nextToken()) - 1;
+            int d = Integer.parseInt(st.nextToken());
 
             int f = sum[d] - sum[c];
             System.out.println(f);
