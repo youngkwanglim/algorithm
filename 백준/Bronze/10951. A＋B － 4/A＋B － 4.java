5605093b55da2c3368283aa7633d8d999b8b42c4
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-06-03 12:18:41 +0900

    [Bronze V] Title: A+B - 4, Time: 120 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            int a = line.charAt(0) - 48;
+            int b = line.charAt(2) - 48;
+            sb.append(a + b).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
