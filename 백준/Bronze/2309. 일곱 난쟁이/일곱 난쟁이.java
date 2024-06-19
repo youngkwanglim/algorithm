3418f6b729cc3dcbacfd658cea2189038bd94e7c
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-06-19 14:07:41 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 112 ms, Memory: 12912 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean tf = false;
+
+        ArrayList<Integer> people = new ArrayList<Integer>();
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            int a = sc.nextInt();
+            people.add(a);
+            sum += a;
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - people.get(i) - people.get(j) == 100) {
+                    people.remove(j);
+                    people.remove(i);
+                    tf = true;
+                    break;
+                }
+            }
+            if (tf) {
+                Collections.sort(people);
+                break;
+            }
+        }
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(people.get(i));
+        }
+    }
+}
