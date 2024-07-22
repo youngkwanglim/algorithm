7fcd3b93bfe2f308846e2f30b47c7a293d3b5bb2
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-07-22 13:32:11 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,13 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int a[9], sum;
-
+int a[9], sum, index1, index2;
 int main(){
-    ios_base::sync_with_stdio(false); 
-    cin.tie(NULL); 
-    cout.tie(NULL);
-    
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     for (int i = 0; i < 9; i++){
         cin >> a[i];
         sum += a[i];
@@ -15,16 +10,13 @@ int main(){
 
     sort(a, a + 9);
 
-    int index1 = -1, index2 = -1;
     for (int i = 0; i < 9; i++){
-        for (int j = i + 1; j < 9; j++){
-            if (sum - a[i] - a[j] == 100){
+        for(int j = i + 1; j < 9; j++){
+            if(sum - a[i]- a[j] == 100){
                 index1 = i;
                 index2 = j;
-                break;
             }
         }
-        if (index1 != -1) break;
     }
 
     for (int i = 0; i < 9; i++){
