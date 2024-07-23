b72698a64bddb2ee0dc6a04f2d35051b930d4030
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-07-23 12:54:34 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -1,23 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, k;
-string s, input, s1, s2;
+int n, pos;
+string s, input, pre, suf;
 int main(){
     cin >> n;
     cin >> s;
-    k = s.find("*");
-    s1 = s.substr(0, k), s2 = s.substr(k + 1, s.length() - k - 1);
+    pos = s.find("*");
+    pre = s.substr(0, pos), suf = s.substr(pos + 1);
 
     for (int i = 0; i < n; i++) {
         cin >> input;
-        if (input.length() >= s1.length() + s2.length() && // Ensure input is long enough
-            input.substr(0, k) == s1 && 
-            input.substr(input.length() - s2.length(), s2.length()) == s2) {
+        if (pre.size() + suf.size() <= input.size() && 
+            input.substr(0, pos) == pre && 
+            input.substr(input.size() - suf.size()) == suf){
             cout << "DA" << '\n';
         }
-        else {
-            cout << "NE" << '\n';
-        }
+        else cout << "NE" << '\n';
     }
 
     return 0;
