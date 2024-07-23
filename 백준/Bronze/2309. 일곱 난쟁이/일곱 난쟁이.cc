7fa481cab1ea07529068a56acb5b95c3bd8e146f
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-07-23 09:27:32 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,27 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-int a[9], sum, index1, index2;
+int sum, child[9], first, second;
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     for (int i = 0; i < 9; i++){
-        cin >> a[i];
-        sum += a[i];
+        cin >> child[i];
+        sum += child[i];
     }
-
-    sort(a, a + 9);
+    sort(child, child + 9);
 
     for (int i = 0; i < 9; i++){
-        for(int j = i + 1; j < 9; j++){
-            if(sum - a[i]- a[j] == 100){
-                index1 = i;
-                index2 = j;
+        for (int j = i + 1; j < 9; j++){
+            if (sum - child[i] - child[j] == 100){
+                first = i; second = j;
             }
         }
     }
 
     for (int i = 0; i < 9; i++){
-        if (i == index1 || i == index2) continue;
-        cout << a[i] << '\n';
+        if(i != first && i != second) cout << child[i] << '\n';
     }
 
     return 0;
