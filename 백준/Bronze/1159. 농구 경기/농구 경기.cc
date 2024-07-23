01ef9a46b692d6660c4a4c733f82e834d432dd98
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-07-23 09:57:53 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.cc
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -1,25 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, cnt[26];
 string s, ret;
+int n, cnt[26];
 int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    // 선수 수(n)을 입력받고
     cin >> n;
-    // 그 이름들을 입력받고
-    for (int i = 0 ; i < n ; i++) {
+    for (int i = 0; i < n; i++){
         cin >> s;
-        // 성의 첫글자 카운트 한다.
         cnt[s[0] - 'a']++;
     }
-    // 그리고 5명 이상이면 이름 사전순으로 출력하고
+
     for (int i = 0; i < 26; i++){
-        if (cnt[i] >= 5) { 
-            ret += i + 'a';
-        }
+        if (cnt[i] >= 5) ret += i + 'a';
     }
-    if (ret.size()) cout << ret <<'\n';
-    // 5명 미만이면 "PREDAJA" 출력.
-    else cout << "PREDAJA" <<'\n';;
-    return 0;
+
+    if (ret.size()) cout << ret << '\n';
+    else cout << "PREDAJA" << '\n';
 }
