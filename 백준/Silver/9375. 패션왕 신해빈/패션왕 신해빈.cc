a9a10e4796a1a9ac1eea11eed91c7d819372f93e
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-07-25 10:30:54 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, m, count[30], ret;
+int n, m, ret;
 string s1, s2;
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
@@ -15,6 +15,7 @@ int main(){
             mp[s2]++; // 이거 처음 접근해서 0이 된뒤에 ++해서 1되는지 확인 해보기
         }
         // 의상 수를 조합으로 계산해서 -1 해주면 됨.(단 같은 종류 의상은 하나만 가능)
+        // -> 종류별로 나누고 종류별 갯수 +1 해줘서 곱해준다음 마지막에 -1만 해주면됨.
         if (mp.size() == 0) cout << 0 << '\n';
         else{
             ret = 1;
@@ -24,7 +25,6 @@ int main(){
             }
             cout << ret - 1  << '\n';
         }
-        // -> 종류별로 나누고 종류별 갯수 +1 해줘서 곱해준다음 마지막에 -1만 해주면됨.
     }
     return 0;
 }
