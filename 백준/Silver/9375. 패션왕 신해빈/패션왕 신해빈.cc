7a68e1ae0a523f76571f687928cf37229bc34721
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-07-27 21:18:29 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -1,30 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int n, m, ret;
-string s1, s2;
+int t, n;
+string a, b;
 int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    // 테스트 케이스 갯수 입력
-    cin >> n;
-    // 의상 수 입력 후 의상이름과 종류 입력
-    for (int i = 0; i < n; i++){
-        cin >> m;
-        map<string, int> mp;
-        for (int j = 0; j < m; j++){
-            cin >> s1 >> s2;
-            mp[s2]++; // 이거 처음 접근해서 0이 된뒤에 ++해서 1되는지 확인 해보기
+    cin >> t;
+    while(t--){
+        map<string, int> _map;
+        cin >> n;
+         for(int i = 0; i < n; i++){
+            cin >> a >> b;
+            _map[b]++;
         }
-        // 의상 수를 조합으로 계산해서 -1 해주면 됨.(단 같은 종류 의상은 하나만 가능)
-        // -> 종류별로 나누고 종류별 갯수 +1 해줘서 곱해준다음 마지막에 -1만 해주면됨.
-        if (mp.size() == 0) cout << 0 << '\n';
-        else{
-            ret = 1;
-            for (auto it : mp){
-                ret *= (it.second + 1);
-                //cout << "ret : " << ret << '\n';
-            }
-            cout << ret - 1  << '\n';
+        long long ret = 1;
+        for(auto c : _map){
+            ret *= ((long long)c.second + 1);
         }
+        ret--;
+        cout << ret << "\n";
     }
     return 0;
 }
