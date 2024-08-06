4c91bf0226203a50e18c92e30c96c18be8be83ef
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-08-06 21:21:14 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -1,13 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 string s;
-int cnt, cnt_mo;
+int lcnt, vcnt;
+char c, prev;
 
-int check(char c){
-	if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
-		return 1;
-	}
-	return 0;
+bool check(char c){
+	return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
 }
 
 int main(){
@@ -15,46 +13,26 @@ int main(){
 	// 모음 하나 이상
 	// 모음이나 자음 연속 3개는 안됨
 	// 같은 글자 두번 이상 안됨(ee와 oo는 허용)
-	cin >> s;
-	while(s != "end"){
-		cnt = 0;
-		cnt_mo = 0;
-		for (int i = 0; i < s.size(); i++){
-			if(check(s[i])) cnt_mo++;
-		}
-		//cout << cnt_mo << " ";
-		if (cnt_mo < 1) {
-			cnt++;
-		}
-		if(s.size() >= 3){
-			for (int i = 0; i < s.size() - 2; i++){
-				if(check(s[i])){
-					if(check(s[i + 1])){
-						if(check(s[i + 2])){
-							cnt++;
-						}
-					}
-				}
-				else{
-					if(check(s[i + 1]) == 0){
-						if(check(s[i + 2]) == 0){
-							cnt++;
-						}
-					}
-				}
-			}
-		}
-		if(s.size() >= 2){
-			for (int i = 0; i < s.size() - 1; i++){
-				if (s[i] == s[i + 1] && s[i] != 'e' && s[i] != 'o'){
-					cnt++;
-					break;
-				}
-			}
-		}
-		//cout << "cnt 갯수 : " << cnt << " ";
-		if (cnt == 0) cout << "<" << s << ">" << " is acceptable.\n";
-		else cout << "<" << s << ">" << " is not acceptable.\n";
+	while(true){
 		cin >> s;
+		if(s == "end")break;
+		lcnt = vcnt = 0;
+		bool flag = 0;
+		bool is_include_v = 0;
+		int prev = -1;
+
+		for (int i = 0; i < s.size(); i++){
+			c = s[i];
+			if(check(c)) lcnt++, vcnt = 0, is_include_v = 1;
+			else vcnt++, lcnt = 0;
+			if(vcnt == 3 || lcnt == 3) flag = 1;
+			if(i >= 1 && (prev == c) && (c != 'e' && c != 'o')){
+				flag = 1;
+			}
+			prev = c;
+		}
+		if(is_include_v == 0) flag = 1;
+		if (flag) cout << "<" << s << ">" << " is not acceptable.\n";
+		else cout << "<" << s << ">" << " is acceptable.\n";
 	}
 }
