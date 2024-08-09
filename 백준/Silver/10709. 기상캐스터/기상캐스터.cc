76a80b180508e8dc435ba7a74f9698481a42e451
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-08-09 22:53:21 +0900

    [Silver V] Title: 기상캐스터, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.cc
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.cc
@@ -1,30 +1,26 @@
-#include <bits/stdc++.h>
-using namespace std;  
-int n, m, a[104][104];
-string s; 
-int main () {
-	cin >> n >> m; 
-	for(int i = 0; i < n; i++){
-		cin >> s; 
-		for(int j = 0; j < m; j++){ 
-			if(s[j] == '.')a[i][j] = -1;
-			else a[i][j] = 0;
+#include<bits/stdc++.h>
+using namespace std;
+int h, w, ret[100][100];
+char a[100][100];
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> h >> w;
+	for(int i = 0; i < h; i++){
+		for(int j = 0; j < w; j++){
+			cin >> a[i][j];
+			ret[i][j] = -1;
+			for(int k =0; k < w; k++){
+				if(j - k >= 0 && a[i][j - k] == 'c'){
+					ret[i][j] = k;
+					break;
+				}
+			}
 		}
 	}
-	for(int i = 0; i < n; i++){ 
-		for(int j = 0; j < m; j++){
-			if(a[i][j] == 0){ 
-				int cnt = 1;
-				while(a[i][j + 1] == -1){
-					a[i][j + 1] = cnt++;
-					j++;
-				} 
-			}
-		} 
-	} 
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < m; j++) cout << a[i][j] << " ";
-		cout << "\n";
-	} 
-	return 0;
+	for(int i = 0; i < h; i++){
+		for(int j = 0; j < w; j++){
+			cout << ret[i][j] << " ";
+		}
+		cout << '\n';
+	}
 }
