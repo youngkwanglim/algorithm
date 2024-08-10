2365bde802f93e3cb1369eb169b25df69ef0dd1d
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-08-10 15:49:35 +0900

    [Silver III] Title: 교수가 된 현우, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -1,20 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, m, cnt2;
-int go(int m){
-    int cnt = 0;
-    for (int i = 5; m / i >= 1; i *= 5) {
-        cnt += m / i;
-    }
-    return cnt;
-}
-
+int n, m, cnt;
 int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> n;
 	for(int i = 0; i < n; i++){
 		cin >> m;
-		cnt2 = go(m);
-		cout << cnt2 << '\n';
+		cnt = 0;
+		for(int j = 5; j <= m; j *= 5){
+			cnt += m / j; 
+		}
+		cout << cnt << '\n';
 	}
 }
