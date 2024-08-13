f1477e8b9e92501f301ec9812d338763f4fac68b
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-08-13 17:00:39 +0900

    [Silver III] Title: NBA 농구, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.cc
+++ 백준/Silver/2852. NBA 농구/NBA 농구.cc
@@ -1,33 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define prev ppppppp
-int n, o, A, B, asum, bsum;
-string s, prev; 
+#define prev kk
+int n, t, A, B, asum, bsum;
+string prev, s;
 string print(int a){ 
-    string d = "00" + to_string(a / 60); 
-    string e = "00" + to_string(a % 60); 
+    string d = "0" + to_string(a / 60); 
+    string e = "0" + to_string(a % 60); 
     return d.substr(d.size() - 2, 2) + ":" + e.substr(e.size() - 2, 2); 
 }
-int changeToInt(string a){
-    return atoi(a.substr(0, 2).c_str()) * 60 + atoi(a.substr(3, 2).c_str());
+int change(string a){
+   return stoi(a.substr(0, 2)) * 60 + stoi(a.substr(3, 2));
 }
-void go(int &sum, string s){ 
-    sum += (changeToInt(s) - changeToInt(prev)); 
+void go(int &sum, string s){
+   sum += (change(s) - change(prev));
 }
 int main(){
-    ios_base::sync_with_stdio(false); 
-    cin.tie(NULL); cout.tie(NULL); 
-    cin >> n; 
-    for(int i = 0; i < n; i++){
-        cin >> o >> s; 
-        if(A > B)go(asum, s);
-        else if(B > A)go(bsum, s);
-		o == 1 ? A++ : B++;
-        prev = s; 
-    } 
-    if(A > B)go(asum, "48:00");
+   ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+   cin >> n;
+   for(int i = 0; i < n; i++){
+		cin >> t >> s;
+		if(A > B) go(asum, s);
+		else if(B > A) go(bsum, s);
+		if(t == 1) A++;
+		else B++;
+		prev = s;
+	}
+	if(A > B)go(asum, "48:00");
     else if(B > A)go(bsum, "48:00"); 
     cout << print(asum) << "\n"; 
     cout << print(bsum) << "\n";
-    return 0;
 }
