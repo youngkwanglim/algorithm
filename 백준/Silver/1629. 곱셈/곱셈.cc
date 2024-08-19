3ff620f282ded8ea4455e726d68efb65254e2a59
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-08-19 23:52:44 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -2,17 +2,16 @@
 using namespace std;
 typedef long long ll;
 ll a, b, c;
-ll go(ll a, ll b){
+int go(ll a, ll b){
 	if (b == 1) return a % c;
-
-	ll ret = go(a, b / 2);
+	ll ret = go (a, b / 2);
 	ret = (ret * ret) % c;
-	if (b % 2) ret = (ret * a) % c;
+	if(b % 2) ret = (ret * a) % c;
 	return ret;
 }
+
 int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> a >> b >> c;
 	cout << go(a, b) << '\n';
-	return 0;
 }
