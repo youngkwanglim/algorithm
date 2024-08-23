9768b9b6552a19d9173644aa75d6e442ca33922f
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-08-23 16:20:25 +0900

    [Gold IV] Title: 인구 이동, Time: 76 ms, Memory: 2240 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.cc
+++ 백준/Gold/16234. 인구 이동/인구 이동.cc
@@ -1,52 +1,60 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int visited[54][54], a[54][54], n, l, r, sum, cnt; 
-const int dy[]={-1,0,1,0};
-const int dx[] ={0,1,0,-1}; 
-vector<pair<int,int>>v;
-void dfs(int y,int x,vector<pair<int,int>> &v){ 
-    for(int i=0; i<4; i++){
-        int ny = y+dy[i];
-        int nx = x+dx[i];
-        if(nx<0 || nx>=n || ny<0 || ny>=n || visited[ny][nx])continue;
-        if(abs(a[ny][nx]- a[y][x]) >= l && abs(a[ny][nx] - a[y][x]) <= r){
-            visited[ny][nx] =1;
-            v.push_back({ny,nx});
-            sum += a[ny][nx];
-            dfs(ny,nx,v);
+
+int n, l, r, a[104][104], visited[104][104], ret, sum;
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+vector<pair<int, int>> v; 
+
+void changeArea(){
+    for(pair<int, int> p : v){
+        a[p.first][p.second] = sum / v.size();
+    }
+}
+
+void dfs(int y, int x){
+    visited[y][x] = 1;
+    v.push_back({y, x});
+    sum += a[y][x];
+    for(int i = 0; i < 4; i++){
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(ny < 0 || ny >= n || nx < 0 || nx >= n || visited[ny][nx]) continue;
+        if(abs(a[ny][nx] - a[y][x]) >= l && abs(a[ny][nx] - a[y][x]) <= r){
+            dfs(ny, nx);
         }
     }
 }
 
-int main(){ 
-    cin>>n>>l>>r;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cin>>a[i][j];
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> l >> r;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> a[i][j];
         }
     }
+
     while(true){
-        bool flag =0;
-        fill(&visited[0][0], &visited[0][0] + 54 * 54, 0);
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+        fill(&visited[0][0], &visited[0][0] + 104*104, 0);  // visited 배열 초기화
+        int flag = 0;  // 변경 발생 여부 확인
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
                 if(!visited[i][j]){
                     v.clear();
-                    visited[i][j] = 1;
-                    v.push_back({i,j});
-                    sum = a[i][j];
-                    dfs(i,j,v);
-                    if(v.size() == 1) continue;  
-                    for(pair<int,int> b : v){ 
-                        a[b.first][b.second] = sum / v.size();
+                    sum = 0;
+                    dfs(i, j);
+                    if(v.size() > 1){  // 2개 이상의 칸이 연결되었을 때만 변경
+                        changeArea();
                         flag = 1;
                     }
-                } 
+                }
             }
         }
-        if(!flag) break;  
-        cnt++;
-    } 
-    cout<< cnt << "\n";
-    return 0;
+        if(!flag) {  // 변경이 없으면 종료
+            cout << ret << '\n';
+            break;
+        }
+        ret++;
+    }
 }
