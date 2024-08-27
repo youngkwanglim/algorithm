55727655a35ecdc4adb711ebe4c83141b17f3618
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-08-27 20:57:33 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 0 ms, Memory: 2932 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -1,29 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, k, visited[100004], ret;
+int n, k, visited[100004], cnt[100004];
 int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> n >> k;
 	queue<int> q;
 	q.push(n);
 	visited[n] = 1;
+	cnt[n] = 1;
 	while(q.size()){
-		int a = q.front();
-		q.pop();
-		if(a + 1 <= 100000 && (!visited[a + 1] || visited[a + 1] == visited[a] + 1)){
-			visited[a + 1] = visited[a] + 1;
-			q.push(a + 1);
-			if(a + 1 == k) ret++;
-		} 
-		if(a - 1 >= 0 && (!visited[a - 1] || visited[a - 1] == visited[a] + 1)){
-			visited[a - 1] = visited[a] + 1;
-			q.push(a - 1);
-			if(a - 1 == k) ret++;
-		}
-		if(2 * a <= 100000 && (!visited[2 * a] || visited[2 * a] == visited[a] + 1)){
-			visited[2 * a] = visited[a] + 1;
-			q.push(a * 2);
-			if(2 * a == k) ret++;
+		int a = q.front(); q.pop();
+		for(int next : {a - 1, a + 1, a * 2}){
+			if(0 <= next && next <= 100000){
+				if(!visited[next]){
+					q.push(next);
+					visited[next] = visited[a] + 1;
+					cnt[next] += cnt[a];
+				} else if(visited[next] == visited[a] + 1){
+					cnt[next] += cnt[a];
+				}
+			}
 		}
 	}
 	if(n == k) {
@@ -32,6 +28,6 @@ int main(){
 	}
 	else{
 		cout << visited[k] - 1 << '\n';
-		cout << ret << '\n';
+		cout << cnt[k] << '\n';
 	}
 }
