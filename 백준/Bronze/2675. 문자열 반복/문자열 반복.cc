a2abae90a18905ea0dd914cd81dcbe64ecd9b92c
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-08-28 10:44:31 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -7,9 +7,9 @@ int main(){
 	for(int i = 0; i < n; i++){
 		cin >> m;
 		cin >> s;
-		for(int j = 0; j < s.length(); j++){
+		for(char c : s){
 			for(int k = 0; k < m; k++){
-				cout << s[j];
+				cout << c;
 			}
 		}
 		cout << '\n';
