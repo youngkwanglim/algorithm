d0752f50ce955354e81f21806ade379f53ca9ae2
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-09-05 23:42:43 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+string s;
+int main(){
+    cout << "\\    /\\" << '\n';
+    cout << " )  ( ')" << '\n';
+    cout << "(  /  )" << '\n';
+    cout << " \\(__)|" << '\n';
+}
