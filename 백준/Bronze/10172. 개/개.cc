e9fce41fcb8ba35985bb8b0b6d82770af1ce5f9d
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-09-05 23:39:33 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+string s;
+int main(){
+    cout << "|\\_/|" << '\n';
+    cout << "|q p|   /}" << '\n';
+    cout << "( 0 )\"\"\"\\" << '\n';
+    cout << "|\"^\"`    |" << '\n';
+    cout << "||_/=\\\\__|" << '\n';
+}
