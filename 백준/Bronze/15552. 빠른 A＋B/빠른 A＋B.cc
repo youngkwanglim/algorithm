e6c1d6a44d60063fa8d5e2cceb614cd4904f6cf7
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-09-09 22:16:39 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 184 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b, c;
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> a;
+    for(int i = 0; i < a; i++){
+        cin >> b >> c;
+        cout << b + c << '\n';
+    }
+}
