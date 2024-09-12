9d29fad32e92c9822de8935c3f8aa3e5fceb46a1
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-09-13 00:09:02 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a, b;
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    while(cin >> a >> b){
+        cout << a + b << '\n';
+    }
+}
