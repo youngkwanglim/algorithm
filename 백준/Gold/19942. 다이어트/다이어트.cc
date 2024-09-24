15cdb7776fda4be5ef3f0a78a509ddc2c14a0583
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-09-24 23:30:05 +0900

    [Gold IV] Title: 다이어트, Time: 8 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.cc
+++ 백준/Gold/19942. 다이어트/다이어트.cc
@@ -1,47 +1,46 @@
 #include<bits/stdc++.h>
-using namespace std;  
-typedef long long ll; 
-const int INF = 987654321;
-int n, mp, mf, ms, mv;
-int b, c, d, e, ret = INF, sum;
-struct A{
-	int mp, mf, ms, mv, cost; 
-}a[16]; 
-map<int, vector<vector<int>>> ret_v;
+using namespace std;
+int n, ze[20][20], ret = 987654321, p, f, s, v, c;
+vector<int> rett;
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> n; 
-    cin >> mp >> mf >> ms >> mv;  
+    cin >> n >> p >> f >> s >> v;
     for(int i = 0; i < n; i++){
-    	cin >> a[i].mp >> a[i].mf >> a[i].ms >> a[i].mv >> a[i].cost;  
-	}
-	for(int i = 1; i < (1 << n); i++){
-		b = c = d = e = sum = 0;
-        vector<int> v; 
-		for(int j = 0; j < n; j++){
-			if(i & (1 << j)){
-                v.push_back(j + 1);
-				b += a[j].mp;
-				c += a[j].mf;
-				d += a[j].ms;
-				e += a[j].mv;
-				sum += a[j].cost;
-			}
-		}
-		if(b >= mp && c >= mf && d >= ms && e >= mv){
-			if(ret >= sum){
-				ret = sum;
-                ret_v[ret].push_back(v); 
-			}
-		} 
-	}  
-	if(ret == INF) cout << -1 << '\n';
-	else{
-        sort(ret_v[ret].begin(), ret_v[ret].end());  
-		cout << ret << "\n";
-		for(int a : ret_v[ret][0]){
-			cout << a << " ";
-		} 
-	}  
-	return 0; 
+        for(int j = 0; j < 5; j++){
+            cin >> ze[i][j];
+        }
+    }
+
+    // 비트마스킹으로 출력
+    for(int i = 0; i < (1 << n); i++){
+        vector<int> vv;
+        int np = 0, nf = 0, ns = 0, nv = 0, nc = 0;
+        for(int j = 0; j < n; j++){
+            if(i & (1 << j)){
+                vv.push_back(j);
+                np += ze[j][0];
+                nf += ze[j][1];
+                ns += ze[j][2];
+                nv += ze[j][3];
+                nc += ze[j][4];
+            }
+        }
+        if(np >= p && nf >= f && ns >= s && nv >= v){
+            if(ret > nc || (ret == nc && vv < rett)){
+                ret = nc;
+                rett = vv;
+            }
+        }
+    }
+
+    if(ret == 987654321){
+        cout << -1;
+        return 0;
+    }
+    // 마지막에 +1씩 해서 출력해야됨.
+    //sort(rett.begin(), rett.end());
+    cout << ret << '\n';
+    for(int i : rett){
+        cout << i + 1 << " ";
+    }
 }
