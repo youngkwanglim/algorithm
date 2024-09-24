54c2cc70c6519af9ce1025ea9d1e232b2303793d
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-09-24 14:12:33 +0900

    [Silver I] Title: 컴백홈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.cc
+++ 백준/Silver/1189. 컴백홈/컴백홈.cc
@@ -1,34 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = {0, 1, 0, -1};
-int n, m, k, visited[10][10]; 
-char a[10][10];
-string s; 
-int go(int y, int x){
-	if(y == 0 && x == m - 1){
-		if(k == visited[y][x]) return 1;
-		return 0; 
-	}
-	int ret = 0; 
-	for(int i = 0; i < 4; i++){
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if(ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx] || a[ny][nx] == 'T')continue;
-		visited[ny][nx] = visited[y][x] + 1; 
-		ret += go(ny, nx);
-		visited[ny][nx] = 0;
-	}
-	return ret; 
+int r, c, k, ret, visited[9][9];
+char a[9][9];
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+void go(int y, int x, int psum){
+    if(y == 0 && x == c - 1 && psum == k){
+        ret++;
+        return;
+    }
+    for(int i = 0; i < 4; i++){
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
+        if(visited[ny][nx]) continue;
+        if(a[ny][nx] == 'T') continue;
+        visited[ny][nx] = 1;
+        //cout << ny << ", " << nx << '\n';
+        go(ny, nx, psum + 1);
+        visited[ny][nx] = 0;
+    }
 }
 int main(){
-	cin >> n >> m >> k; 
-	for(int i = 0; i < n; i++){
-		cin >> s; 
-		for(int j = 0; j < m; j++){
-			a[i][j] = s[j];
-		}
-	} 
-	visited[n - 1][0] = 1; 
-	cout << go(n - 1, 0) << "\n";
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> r >> c >> k;
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            cin >> a[i][j];
+        }
+    }
+    // 마지막에 도착했을 떄 k랑 거리가 같으면 ret을 증가시켜주면 됨.
+    if(a[r - 1][0] == 'T' || a[0][c - 1] == 'T'){
+        cout << 0;
+        return 0;
+    }
+    visited[r - 1][0] = 1;
+    go(r - 1, 0, 1);
+    cout << ret;
 }
