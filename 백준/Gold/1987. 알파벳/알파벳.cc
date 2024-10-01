c9858d97cfbeb13e964a72bee5ffdeb0163c61ae
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-10-01 15:25:51 +0900

    [Gold IV] Title: 알파벳, Time: 368 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,37 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int MAX = 20;
+int n, m, ret;
+char a[24][24];
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
-int r, c, visited[MAX + 4][MAX + 4], cnt[26], ret = -1;
-char a[MAX + 4][MAX + 4];
-void dfs(int y, int x, int dist){
-    ret = max(dist, ret);
+void dfs(int y, int x, int num, int step){
+    ret = max(ret, step);
     for(int i = 0; i < 4; i++){
         int ny = y + dy[i];
         int nx = x + dx[i];
-        if(ny < 0 || ny >= r || nx < 0 || nx >= c || visited[ny][nx]) continue;
-        if(cnt[a[ny][nx] - 'A']) continue;
-        //q.push({ny, nx});
-        visited[ny][nx] = 1;
-        cnt[a[ny][nx] - 'A']++;
-        dfs(ny, nx, dist + 1);
-        visited[ny][nx] = 0;
-        cnt[a[ny][nx] - 'A']--;
+        if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+        int _next = (1 << (a[ny][nx] - 'A'));
+        if((num & _next) == 0) dfs(ny, nx, num | _next, step + 1);
     }
 }
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> r >> c;
-    for(int i = 0; i < r; i++){
-        for(int j = 0; j < c; j++){
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
             cin >> a[i][j];
         }
     }
-    visited[0][0] = 1;
-    cnt[a[0][0] - 'A']++;
-    dfs(0, 0, 1);
-
+    dfs(0, 0, 1 << (a[0][0] - 'A'), 1);
     cout << ret << '\n';
 }
