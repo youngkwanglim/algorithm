b5226a111a369bffadecfdacbfaa6135d6df390b
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-10-02 11:49:21 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int x, tmp, ret;
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> x;
+    for(int i = 6; i >= 0; i--){
+        if((1 << i) <= x){
+            tmp = i;
+            break;
+        }
+    }
+    //cout << tmp << '\n';
+    for(int i = 0; i <= tmp; i++){
+        if(((1 << i) & x) == (1 << i)) ret++;
+    }
+    cout << ret << '\n';
+}
