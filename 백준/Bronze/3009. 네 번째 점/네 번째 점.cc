2ac6c9414627a83f85fe98d10da4614dd9b7653c
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-10-23 17:38:15 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define y1 xxxxxx
+int x1, x2, x3, x4, y1, y2, y3, y4;
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+    if(x1 == x2) x4 = x3;
+    else if(x1 == x3) x4 = x2;
+    else x4 = x1;
+
+    if(y1 == y2) y4 = y3;
+    else if(y1 == y3) y4 = y2;
+    else y4 = y1;
+    cout << x4 << " " << y4 << '\n';
+}    
