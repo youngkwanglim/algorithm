7cbb1a478534442836c3cabfd78973124eb59e7d
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-10-24 12:24:45 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.cc
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.cc
@@ -1,29 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-int a[3], _max, _max_i, tmp1, tmp2;
+int a[3];
 string s;
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     while(true){
-        cin >> a[0] >> a[1] >> a[2];
+        for(int i = 0; i < 3; i++){
+            cin >> a[i];
+        }
         if(a[0] == 0 && a[1] == 0 && a[2] == 0) break;
 
-        _max = 0;
-        for(int i = 0; i < 3; i++){
-            if(_max < a[i]){
-                _max = a[i];
-                _max_i = i;
-            }
-        }
-        
-        int sum =0;
-        for(int i = 0; i < 3; i++){
-            if(i != _max_i) sum += a[i];
-        }
+        sort(a, a + 3);
 
-        if(_max >= sum){
-            s = "Invalid";
-        }
+        if(a[2] >= a[1] + a[0]) s = "Invalid";
         else if(a[0] == a[1] && a[1] == a[2]) s = "Equilateral";
         else if(a[0] != a[1] && a[1] != a[2] && a[2] != a[0]) s =  "Scalene";
         else s = "Isosceles";
