54630a489eb687323b4978b3c45822d13e00ba51
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-10-29 14:56:39 +0900

    [Silver III] Title: 분수 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.cc
+++ 백준/Silver/1735. 분수 합/분수 합.cc
@@ -1,9 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 int a1, a2, b1, b2;
-ll ret1, ret2;
-ll gcd(ll a, ll b){
+int gcd(int a, int b){
     if(a == 0) return b;
     return gcd(b % a, a);
 }
@@ -11,9 +9,9 @@ ll gcd(ll a, ll b){
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> a1 >> a2 >> b1 >> b2;
-    ll tmp1 = a1 * b2 + b1 * a2;
-    ll tmp2 = a2 * b2;
-    ll tmp3 = 0;
+    int tmp1 = a1 * b2 + b1 * a2;
+    int tmp2 = a2 * b2;
+    int tmp3 = 0;
     if (tmp1 > tmp2) tmp3 = gcd(tmp2, tmp1);
     else tmp3 = gcd(tmp1, tmp2);
     cout << tmp1 / tmp3 << " " << tmp2 / tmp3 << '\n';;
