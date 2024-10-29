23fe1203fe19cdd019ce2bcf095a283d24a27c4a
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-10-29 14:22:54 +0900

    [Bronze I] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.cc
+++ 백준/Bronze/1934. 최소공배수/최소공배수.cc
@@ -1,17 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int a, b, n, _min;
-
+int gcd(int a, int b){
+    if(a == 0) return b;
+    return gcd(b % a, a);
+}
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n;
     while(n--){
         cin >> a >> b;
-        _min = min(a, b);
-        int tmp = 1;
-        for(int i = 2; i <= _min; i++){
-            if(a % i == 0 && b % i == 0) tmp = i;
-        }
-        cout << a * b / tmp << '\n';
+        cout << a * b / gcd(a, b) << '\n';
     }
 }
