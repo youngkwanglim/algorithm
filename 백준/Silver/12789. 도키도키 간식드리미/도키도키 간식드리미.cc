e9d7c356f4a1d46bdd679f44de387f04579859a7
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-10-31 14:39:26 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
@@ -1,35 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, a;
-queue<int> q;
-stack<int> st2;
+int n, a, cnt = 1;
+stack<int> st;
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n;
     for(int i = 0; i < n; i++){
         cin >> a;
-        q.push(a);
+        if(cnt == a) cnt++;
+        else st.push(a);
+        while(st.size() && st.top() == cnt){
+            st.pop();
+            cnt++;
+        }        
     }
-    for(int i = 1; i <= n; ){
-        if(st2.size() && st2.top() == i){
-            st2.pop();
-            i++;
-            continue;
-        }
-        else if(q.size() && q.front() == i){
-            q.pop();
-            i++;
-            continue;
-        }
-        else if(q.size()){
-                st2.push(q.front());
-                q.pop();
-        }
-        else{
-            break;
-        }
-    }
-    if(q.empty() && st2.empty()) cout << "Nice\n";
+    if(st.empty()) cout << "Nice\n";
     else cout << "Sad\n";
 }
