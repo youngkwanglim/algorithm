c40796b60690b82b961eaf46c0cfa229c55ef8cd
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-11-05 23:52:44 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,19 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, m, a[100004];
+int n, m;
 vector<int> v;
-map<int, int> mp;
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n;
-    for(int i = 0; i < n; i++){
+    while(n--){
         cin >> m;
-        mp[m] = 1;
+        v.push_back(m);
     }
+    sort(v.begin(), v.end());
     cin >> n;
     while(n--){
         cin >> m;
-        if(mp.find(m) == mp.end()) cout << 0 << '\n';
+        if(binary_search(v.begin(), v.end(), m) == 0) cout << 0 << '\n';
         else cout << 1 << '\n';
     }
 }
