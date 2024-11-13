30360f66ddef2d0b1925ce61c04c74909e1dee31
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-11-13 14:23:17 +0900

    [Gold III] Title: 소수의 연속합, Time: 32 ms, Memory: 13740 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -1,55 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, ret;
-vector<int> v;
-bool go(int m){
-    if(m == 0 || m == 1) return 0;
-    if(m == 2) return 1;
-    for(int i = 2; i * i <= m; i++){
-        if(m % i == 0) return 0;
-    }
-    return 1;
-}
-
+bool che[4000004];
+int n, a[2000004], p, lo, hi, ret, sum;
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n;
-    if(n == 1){
-        cout << 0 << '\n';
-        return 0;
-    }
-    if(n == 2){
-        cout << 1 << '\n';
-        return 0;
-    }
-    for(int i = 1; i <= n; i++){
-        if(go(i)) v.push_back(i);
-    }
-    // for(int i : v){
-    //     cout << i << '\n';
-    // }
-    int size = 1;
-    while(true){
-        int flag = 0;
-        for(int j = 0; j < v.size(); j++){
-            int tmp = 0;
-            for(int k = 0; k < size; k++){
-                //cout << j + k << "번째" << v[j + k] << '\n';
-                tmp += v[j + k];
-                //cout << tmp << '\n';
-            }
-            if(j == 0 && tmp > n){
-                flag = 1;
-                break;
-            }
-            if(tmp == n) {
-                ret++;
-                break;
-            }
-            if(tmp > n) break;
+    for(int i = 2; i <= n; i++){
+        if(che[i]) continue;
+        for(int j = 2 * i; j<=n; j+=i){
+            che[j] = 1;
         }
-        size++;
-        if(flag) break;
+    }
+    for(int i = 2; i <= n; i++){
+        if(!che[i]) a[p++] = i;
+    }
+    while(true){
+        if(sum >= n) sum -= a[lo++];
+        else if(hi == p) break;
+        else sum += a[hi++];
+        if(sum == n) ret++;
     }
     cout << ret << '\n';
 }
