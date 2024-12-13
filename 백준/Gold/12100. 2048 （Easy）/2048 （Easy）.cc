e3917085d2058da01f25cf10a31dcb2647bb0fbf
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2024-12-13 17:19:27 +0900

    [Gold I] Title: 2048 (Easy), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
@@ -1,77 +1,60 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int sum, N;
-int board[24][24];
-void rotate(int board[24][24]) {
-    int temp[24][24] = { 0 };
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            temp[i][j] = board[N - 1 - j][i];
-        }
-    }
-    memcpy(board, temp, sizeof(temp));
-}
-
-void slide(int board[24][24]) {
-    int temp[24][24];
-    memset(temp, 0, sizeof(temp));
-    for (int i = 0; i < N; i++) {
-        int cnt = 0;
-        for (int j = 0; j < N; j++) {
-            if (board[i][j] == 0) continue;
-
-            if (temp[i][cnt] == 0) {
-                temp[i][cnt] = board[i][j];
-            } else if (temp[i][cnt] == board[i][j]) {
-                temp[i][cnt] *= 2;
-                cnt++;
-            } else {
-                cnt++;
-                temp[i][cnt] = board[i][j];
+int n, a[24][24], ret;
+struct Board{
+    int a[24][24];
+    void rotate90(){
+        int tmp[24][24];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                tmp[i][j] = a[n - j - 1][i];
             }
         }
-    } 
-    memcpy(board, temp, sizeof(temp));
-}
-
-
-void counting(int board[24][24]) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            sum = max(sum, board[i][j]);
+        copy(&tmp[0][0], &tmp[0][0] + 24*24, &a[0][0]);
+    }
+    void move(){
+        int tmp[24][24];
+        for(int i = 0; i < n; i++){
+            int c = -1, d = 0;
+            for(int j = 0; j < n; j++){
+                if(a[i][j] == 0) continue;
+                if(d && a[i][j] == tmp[i][c]) tmp[i][c] *= 2, d=0;
+                else tmp[i][++c] = a[i][j], d = 1;
+            }
+            for(c++; c < n; c++) tmp[i][c] = 0;
+        }
+        copy(&tmp[0][0], &tmp[0][0] + 24*24, &a[0][0]);
+    }
+    void get_max(){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                ret = max(ret, a[i][j]);
+            }
         }
     }
-}
-
-void go(int here, int board[24][24]) {
-    if (here == 5) {
-        counting(board);
+};
+void go(Board c, int here){
+    if(here == 5){
+        c.get_max();
         return;
-    } 
-    for (int i = 0; i < 4; i++) {
-        int cpy[24][24];
-        memcpy(cpy, board, sizeof(cpy));
-        slide(cpy);
-        go(here + 1, cpy);
-        rotate(board);
     }
+    for(int i = 0; i < 4; i++){
+        Board d = c;
+        d.move();
+        go(d, here + 1);
+        c.rotate90();
+    }
+    return;
 }
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin >> N;  
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cin >> board[i][j];
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    Board c;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> c.a[i][j]; 
         }
     }
-
-    go(0, board);
-
-    cout << sum;
-    return 0;
+    go(c, 0);
+    cout << ret << '\n';
 }
