6d23b6b1330dda5be15bb12462f8f95f69e3b649
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2025-02-16 18:00:46 +0900

    [Gold IV] Title: 뱀, Time: 0 ms, Memory: 2108 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.cc
+++ 백준/Gold/3190. 뱀/뱀.cc
@@ -1,48 +1,44 @@
 #include<bits/stdc++.h>
-using namespace std;  
+using namespace std;
 #define time ff
-#define y1 cc
-typedef long long ll;  
-int n, k, l, y, x, t, ret, idx, dir = 1;
-int a[104][104], visited[104][104], time; 
-char c; 
-deque<pair<int, int>> dq;    
-vector<pair<int, int>> _time; 
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = {0, 1, 0, -1}; 
+int n, k, a[104][104], y, x, dir = 1, visited[104][104], l, tmp, t, idx, time;
+char c;
+deque<pair<int, int>> dq;
+vector<pair<int, int>> _time;
+const int dy[] = {-1, 0 , 1, 0};
+const int dx[] = {0, 1, 0, -1};
 int main(){
-    ios::sync_with_stdio(false);cin.tie(0);
-    cin >> n >> k; 
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> k;
     for(int i = 0; i < k; i++){
-        cin >> y >> x; 
-        a[--y][--x] = 1; 
+        cin >> y >> x;
+        a[y - 1][x - 1] = 1;
     }
-    cin >> l; 
+    cin >> l;
     for(int i = 0; i < l; i++){
-        cin >> t >> c;   
-        if(c == 'D') _time.push_back({t, 1});
-        else _time.push_back({t, 3}); 
-    } 
-    dq.push_back({0, 0}); 
-    while(dq.size()){ 
-        time++; 
-        tie(y, x) = dq.front();
-        int ny = y + dy[dir]; 
-        int nx = x + dx[dir]; 
-        if(ny >= n || ny < 0 || nx >= n || nx < 0 || visited[ny][nx]) break;
-        
-        if(!a[ny][nx]){ 
-            visited[dq.back().first][dq.back().second] = 0;  
-            dq.pop_back(); 
-        }else a[ny][nx] = 0; 
-
-        visited[ny][nx] = 1;  
-        dq.push_front({ny, nx}); 
-        if(time == _time[idx].first){
-            dir = (dir + _time[idx].second) % 4; 
-            idx++; 
-        }    
+        cin >> t >> c;
+        if(c == 'D') _time.push_back({t,1});
+        else _time.push_back({t,3});
     }
-    cout << time << "\n"; 
-    return 0;
+    dq.push_back({0,0});
+    while(dq.size()){
+        time++;
+        tie(y,x) = dq.front();
+        int ny = y + dy[dir];
+        int nx = x + dx[dir];
+        if(ny >= n || ny < 0 || nx >= n || nx < 0 || visited[ny][nx]) break;
+
+        if(!a[ny][nx]){
+            visited[dq.back().first][dq.back().second] = 0;
+            dq.pop_back();
+        } else a[ny][nx] = 0;
+
+        visited[ny][nx] = 1;
+        dq.push_front({ny, nx});
+        if(time == _time[idx].first){
+            dir = (dir + _time[idx].second) % 4;
+            idx++;
+        }
+    }
+    cout << time << '\n';
 }
