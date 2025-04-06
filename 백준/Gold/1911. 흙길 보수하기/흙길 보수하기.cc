062f4d2a9a3c9062b6c160ddc76db73e1f70381c
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2025-04-06 21:27:37 +0900

    [Gold V] Title: 흙길 보수하기, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.cc
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, l, a, b, ret, last;
+vector<pair<int,int>> v;
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin >> n >> l;
+    for(int i = 0; i < n; i++){
+        cin >> a >> b;
+        v.push_back({a,b}); 
+    }
+    sort(v.begin(), v.end());
+    // for(auto it : v){
+    //     a = it.first;
+    //     b = it.second;
+    //     cout << a << " , " << b << '\n';
+    // }
+    for(auto it : v){
+        a = it.first;
+        b = it.second;
+        if(last > a) a = last;
+        int tmp = (b - a) / l;
+        if((b - a) % l > 0) tmp += 1;
+        ret += tmp;
+        last =  a + tmp * l ;
+    }
+    cout << ret << '\n';
+}
