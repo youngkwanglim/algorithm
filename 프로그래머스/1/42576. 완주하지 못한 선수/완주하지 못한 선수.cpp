832ce173ec8caaf36cd469a51d46f3f6e116e0aa
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2025-06-04 18:08:58 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.75 ms, Memory: 23.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -1,27 +1,15 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-map<string, int> m;
 
 string solution(vector<string> participant, vector<string> completion) {
-    string answer = "";
-    for(string p : participant){
-        if(m.find(p) == m.end()){
-            m[p] = 1;
+    // 동명이인 처리가 핵심임.
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    for(int i = 0 ; i < completion.size(); i++){
+        if(completion[i] != participant[i]){
+            return participant[i];
         }
-        else m[p]++;
     }
-    
-    for(string c : completion){
-        if(m[c] == 1){
-            m.erase(c);
-        }
-        else m[c]--;
-    }
-    
-    for(auto it : m){
-        answer = it.first;
-    }
-    
-    return answer;
+    return participant[participant.size() - 1];
 }
