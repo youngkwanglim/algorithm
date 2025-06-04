32f5584a2f74e2daa827612e5b61b876e2321923
youngkwanglim <104816348+youngkwanglim@users.noreply.github.com>
2025-06-04 18:56:51 +0900

    [level 2] Title: 전화번호 목록, Time: 84.83 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -3,14 +3,21 @@
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    sort(phone_book.begin(), phone_book.end());
+    // for(string s : phone_book){
+    //     if(phone_book.find(s) == phone_book.end()) continue;
+    //     else{
+    //         return false;
+    //     }
+    // }
+    // return true;
     
+    sort(phone_book.begin(), phone_book.end());
     for(int i = 0; i < phone_book.size() - 1; i++){
-        if(phone_book[i + 1].find(phone_book[i]) == 0){
-            return false;
-            
+        if(phone_book[i + 1] > phone_book[i]){
+            if(phone_book[i + 1].substr(0, phone_book[i].size()) == phone_book[i]){
+                return false;
+            }
         }
     }
-    
     return true;
 }
